493d90f81066fc8bb36bff4d9e035aa2b68fd749
Adam Sullovey <adam.sullovey@rangle.io>
2020-12-24 01:07:44 -0500

    add a comment

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ rm -rf netlify-automatic-functions dist
 
 # the functions need AWS credentials, but Netlify blocks me from entering variables with names like AWS_REGION in the GUI
 # so set these up another way
+# DB_AWS_REGION, DB_AWS_ACCESS_KEY_ID, and DB_AWS_SECRET_ACCESS_KEY must be configured in Netlify
+# this will put their values into a .env file with the regular names the AWS SDK expects
 # inspired by https://remysharp.com/2019/05/18/aws-inside-netlify
 cat > ./.env <<EOL
 AWS_REGION = $DB_AWS_REGION
